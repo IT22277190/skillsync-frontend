31851024c1121104cca641489ebafac5b153a3e1
IT22082138 <127777616+IT22082138@users.noreply.github.com>
2025-05-05 13:28:46 +0530

    following.

--- src/components/FollowingAccountItem.js
+++ src/components/FollowingAccountItem.js
@@ -1,37 +1,51 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import { Hashicon } from "@emeraldpay/hashicon-react";
 import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { getProfileId } from "../feature/checkProfile/checkProfileSlice";
-import { unfollowAccount } from "../feature/followingAccounts/followingAccountSlice";
+import { followAccount } from "../feature/followingAccounts/followingAccountSlice";
 
 import { Button } from "react-bootstrap";
-import { RiCheckFill, RiDeleteBin6Line } from "react-icons/ri";
+import { RiCheckFill, RiUserFollowFill } from "react-icons/ri";
 
-function FollowingAccountItem(props) {
+function FollowerAccountItem(props) {
   const dispatch = useDispatch();
   const selectedProfileId = useSelector(
     (state) => state.checkProfileReducer.profileId
   );
+  const storeFollowingAccounts = useSelector(
+    (state) => state.followingAccountReducer.followingAccounts
+  );
 
-  const [followButtonTitle, setFollowButtonTitle] = useState("Unfollow");
+  const [followButtonTitle, setFollowButtonTitle] = useState("Follow");
   const [tickIconStatus, setTickIconStatus] = useState(false);
 
   function handleFollowButtonClick(e) {
     dispatch(
-      unfollowAccount({
+      followAccount({
         followedId: props.id,
         followerId: localStorage.getItem("psnUserId"),
       })
     );
-    setFollowButtonTitle("Unfollowed");
+    setFollowButtonTitle("Followed");
     setTickIconStatus(true);
   }
 
   function handleClick(e) {
     dispatch(getProfileId(props.id));
   }
+
+  useEffect(() => {
+    if (storeFollowingAccounts !== null) {
+      for (let i = 0; i < storeFollowingAccounts.length; i++) {
+        if (storeFollowingAccounts[i].id === props.id) {
+          setTickIconStatus(true);
+          setFollowButtonTitle("Followed");
+        }
+      }
+    }
+  }, []);
 
   return (
     <div className="d-flex align-items-center my-5">
@@ -49,15 +63,16 @@ function FollowingAccountItem(props) {
       </div>
       <div>
         <Button
-          variant={tickIconStatus ? "danger" : "success"}
+          variant={tickIconStatus ? "primary" : "success"}
           onClick={handleFollowButtonClick}
+          disabled={tickIconStatus}
         >
           {followButtonTitle}{" "}
-          {tickIconStatus ? <RiCheckFill /> : <RiDeleteBin6Line />}
+          {tickIconStatus ? <RiCheckFill /> : <RiUserFollowFill />}
         </Button>
       </div>
     </div>
   );
 }
 
-export default FollowingAccountItem;
+export default FollowerAccountItem;
