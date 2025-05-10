27dc9d66a5a22b08ec8a2de40c7808256c04d08f
IT22277190 <sudeepasandabhanu@gmail.com>
2025-05-10 17:39:22 +0530

    Add feedback reducer to the Redux store configuration

--- src/app/store.js
+++ src/app/store.js
@@ -2,11 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import followingPostReducer from "../feature/followingPost/followingPostSlice";
 import followingAccountReducer from "../feature/followingAccounts/followingAccountSlice";
 import checkProfileReducer from "../feature/checkProfile/checkProfileSlice";
+import feedbackReducer from "../feature/feedback/feedbackSlice";
+
 
 export const store = configureStore({
     reducer: {
         followingPostReducer: followingPostReducer,
         followingAccountReducer: followingAccountReducer,
         checkProfileReducer: checkProfileReducer,
+        feedbackReducer: feedbackReducer,
     },
 });
